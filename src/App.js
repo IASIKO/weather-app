df3cf30735b8bc39b82ce83e5b7cd5ffa7fc9a91
IASIKO <iaseshviligi@gmail.com>
2023-10-19 21:02:13 +0400

    update alert

--- src/App.js
+++ src/App.js
@@ -6,86 +6,77 @@ import "./App.css";
 function App() {
   const [weatherData, setWeatherData] = useState([]);
   const [location, setLocation] = useState("");
+  const [error, setError] = useState("");
 
   const API_KEY = "3856b67777442241727b3e3f9a82097f";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
 
-  const searchLocation = (event) => {
+  const searchLocation = async (event) => {
     if (event.key === "Enter") {
-      axios
-        .get(url)
-        .then((res) => {
-          setWeatherData(res.data);
-          console.log(res.data);
-        })
-        .catch((error) => {
-          console.log(error);
-          return error;
-        });
+      try {
+        const response = await axios.get(url);
+        setWeatherData(response.data);
+        setError("");
+      } catch (error) {
+        setError("This city does not exist, try another one!");
+      }
       setLocation("");
     }
   };
 
   return (
-    <>
-      {/* {error ? (
-        <Alert />
-      ) : ( */}
-      <div className="app">
-        <div className="search">
-          <input
-            type="text"
-            placeholder="Enter Location"
-            onKeyPress={searchLocation}
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
+    <div className="app">
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Enter Location"
+          onKeyPress={searchLocation}
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+      </div>
+      {error === "" ? null : <Alert error={error} />}
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{weatherData.name}</p>
+          </div>
+          <div className="temp">
+            {weatherData.main ? (
+              <h1>{weatherData.main.temp.toFixed()} C</h1>
+            ) : null}
+          </div>
+          <div className="description">
+            {weatherData.weather ? <p>{weatherData.weather[0].main}</p> : null}
+          </div>
         </div>
-        <div className="container">
-          <div className="top">
-            <div className="location">
-              <p>{weatherData.name}</p>
-            </div>
-            <div className="temp">
+
+        {weatherData.name && (
+          <div className="bottom">
+            <div className="feels">
               {weatherData.main ? (
-                <h1>{weatherData.main.temp.toFixed()} C</h1>
+                <p className="bold">
+                  {weatherData.main.feels_like.toFixed()} C
+                </p>
               ) : null}
+              <p>Feels like</p>
             </div>
-            <div className="description">
-              {weatherData.weather ? (
-                <p>{weatherData.weather[0].main}</p>
+            <div className="humidity">
+              {weatherData.main ? (
+                <p className="bold">{weatherData.main.humidity}%</p>
               ) : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {weatherData.wind ? (
+                <p className="bold">{weatherData.wind.speed.toFixed()} MPH</p>
+              ) : null}
+              <p>Wind speed</p>
             </div>
           </div>
-
-          {weatherData.name && (
-            <div className="bottom">
-              <div className="feels">
-                {weatherData.main ? (
-                  <p className="bold">
-                    {weatherData.main.feels_like.toFixed()} C
-                  </p>
-                ) : null}
-                <p>Feels like</p>
-              </div>
-              <div className="humidity">
-                {weatherData.main ? (
-                  <p className="bold">{weatherData.main.humidity}%</p>
-                ) : null}
-                <p>Humidity</p>
-              </div>
-              <div className="wind">
-                {weatherData.wind ? (
-                  <p className="bold">{weatherData.wind.speed.toFixed()} MPH</p>
-                ) : null}
-                <p>Wind speed</p>
-              </div>
-            </div>
-          )}
-        </div>
+        )}
       </div>
-      {/* )} */}
-    </>
+    </div>
   );
 }
 
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./Alert.css";
 
-const Alert = () => {
-  return <div>Alert</div>;
+const Alert = ({ error }) => {
+  return (
+    <div className="alertContainer">
+      <div className="alert">
+        <p>{error}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Alert;
