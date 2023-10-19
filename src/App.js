2c7397c64fce39698b21260d5bcfa044d32fa239
IASIKO <iaseshviligi@gmail.com>
2023-10-19 20:17:51 +0400

    add alert component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Alert from "./components/Alert";
 import "./App.css";
 
 function App() {
@@ -11,66 +12,80 @@ function App() {
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
-      axios.get(url).then((res) => {
-        setWeatherData(res.data);
-        console.log(res.data);
-      });
+      axios
+        .get(url)
+        .then((res) => {
+          setWeatherData(res.data);
+          console.log(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+          return error;
+        });
       setLocation("");
     }
   };
 
   return (
-    <div className="app">
-      <div className="search">
-        <input
-          type="text"
-          placeholder="Enter Location"
-          onKeyPress={searchLocation}
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-      </div>
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>{weatherData.name}</p>
-          </div>
-          <div className="temp">
-            {weatherData.main ? (
-              <h1>{weatherData.main.temp.toFixed()} C</h1>
-            ) : null}
-          </div>
-          <div className="description">
-            {weatherData.weather ? <p>{weatherData.weather[0].main}</p> : null}
-          </div>
+    <>
+      {/* {error ? (
+        <Alert />
+      ) : ( */}
+      <div className="app">
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Enter Location"
+            onKeyPress={searchLocation}
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </div>
-
-        {weatherData.name && (
-          <div className="bottom">
-            <div className="feels">
-              {weatherData.main ? (
-                <p className="bold">
-                  {weatherData.main.feels_like.toFixed()} C
-                </p>
-              ) : null}
-              <p>Feels like</p>
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>{weatherData.name}</p>
             </div>
-            <div className="humidity">
+            <div className="temp">
               {weatherData.main ? (
-                <p className="bold">{weatherData.main.humidity}%</p>
+                <h1>{weatherData.main.temp.toFixed()} C</h1>
               ) : null}
-              <p>Humidity</p>
             </div>
-            <div className="wind">
-              {weatherData.wind ? (
-                <p className="bold">{weatherData.wind.speed.toFixed()} MPH</p>
+            <div className="description">
+              {weatherData.weather ? (
+                <p>{weatherData.weather[0].main}</p>
               ) : null}
-              <p>Wind speed</p>
             </div>
           </div>
-        )}
+
+          {weatherData.name && (
+            <div className="bottom">
+              <div className="feels">
+                {weatherData.main ? (
+                  <p className="bold">
+                    {weatherData.main.feels_like.toFixed()} C
+                  </p>
+                ) : null}
+                <p>Feels like</p>
+              </div>
+              <div className="humidity">
+                {weatherData.main ? (
+                  <p className="bold">{weatherData.main.humidity}%</p>
+                ) : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {weatherData.wind ? (
+                  <p className="bold">{weatherData.wind.speed.toFixed()} MPH</p>
+                ) : null}
+                <p>Wind speed</p>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      {/* )} */}
+    </>
   );
 }
 
